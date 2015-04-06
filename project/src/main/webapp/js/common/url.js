11a47f448a983d9a1bfb62776099671e481bc55e
Jianjiao Sun <jianjiaosun@gmail.com>
2015-04-06 20:46:15 +0800

    for url parse

---
+++ project/src/main/webapp/js/common/url.js
@@ -0,0 +1,11 @@
+function getUrlParam(name) {  
+    //构造一个含有目标参数的正则表达式对象  
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");  
+    //匹配目标参数  
+    var r = window.location.search.substr(1).match(reg);  
+    //返回参数值  
+    if (r != null)
+    	return unescape(r[2]);
+    
+    return null;  
+}
