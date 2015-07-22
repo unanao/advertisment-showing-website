f8abbc11a3025c567babf4caba683fe612cf7d61
Jianjiao Sun <sunjianjiao@gmail.com>
2015-07-22 14:28:53 +0800

    git login optimize

--- project/src/main/webapp/js/accounts/login.js
+++ project/src/main/webapp/js/accounts/login.js
@@ -1,49 +1,65 @@
-/**
- * 
- */
+var accountsLogin  = {};
 
-$(function() {
-	var  username = false, password = false;
-	function checkUsername() 
-	{
-		if ($("#userName").val().replace(/[ ]/g, "") != "") 
-		{
-			if (/^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/.exec($("#userName").val())) 
-			{
-				$("#euserName").html("");
-				username = true;
-			}
-			else
-			{
-				$("#euserName").html("用户名为您的注册邮箱");
-				username = false;
-			}
-			
-		} 
-		else 
-		{
-			$("#euserName").html("请输入用户名");
-			username = false;
-		}
+$function() {
+	$("#username").blur(accountsLogin.checkUsername);
+	$("#password").blur(accountsLogin.checkPassword);
+}
+
+accountsLogin.loginAuth = function() {
+	var userName = $("#userName").val();	
+	var password = $("#password").val();
+	
+	alert("no");
+	
+	if (true != accountsLogin.checkInput()) {
+		return;
 	}
-	
-	function checkPassword() {
-		if ($("#password").val().replace(/[ ]/g, "") != "") {
-			$("#epassword").html("");
-			password = true;
-		} else {
-			$("#epassword").html("请输入密码");
-			password = false;
-		}
-	}
-	
-	$("#username").blur(checkUsername);
-	$("#password").blur(checkPassword);
-	
-	$("#submit").click(function() {
-		checkUsername();
-		checkPassword();
-		return username && password;
+	alert(userName);
+	alert(password);
+	$.post("accounts/login", {
+		userName:userName,
+		password : password,
+	}, function(data) {
+		alert(hello);
 	});
+}
+
+accountsLogin.checkUsername = function () 
+{
+	if ($("#userName").val().replace(/[ ]/g, "") != "") 
+	{
+		if (/^(\w)+(\.\w+)*@(\w)+((\.\w+)+)$/.exec($("#userName").val())) 
+		{
+			$("#euserName").html("");
+			return true;
+		}
+		else
+		{
+			$("#euserName").html("用户名为您的注册邮箱");
+			return  false;
+		}
+		
+	} 
+	else 
+	{
+		$("#euserName").html("请输入用户名");
+		return false;
+	}
+}
+
+accountsLogin.checkPassword = function() {
+	if ($("#password").val().replace(/[ ]/g, "") != "") {
+		$("#epassword").html("");
+		return true;
+	} else {
+		$("#epassword").html("请输入密码");
+		return false;
+	}
+}
 	
-});
+accountsLogin.checkInput = function() {
+		var userName = checkUsername();
+		var password = checkPassword();
+		return userName && password;
+}
+	
